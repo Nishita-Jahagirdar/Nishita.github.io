9f52319b26f648d6039a36183830eca6e688c820
Nishita P Jahagirdar <01fe23bcs123@kletech.ac.in>
2024-12-28 01:04:26 +0530

    Update priorityqueue.cpp

--- priorityqueue.cpp
+++ priorityqueue.cpp
@@ -3,30 +3,28 @@
 #include <string>
 using namespace std;
 
-struct Request {
+struct Request 
+{
     string zone;
     int severity;
 
-    // Comparator to create a max-heap
     bool operator<(const Request& other) const {
         return severity < other.severity;
     }
 };
 
-int main() {
+int main() 
+{
     priority_queue<Request> pq;
 
-    // Add evacuation requests
     pq.push({"Zone A", 5});
     pq.push({"Zone B", 9});
     pq.push({"Zone C", 3});
 
-    // Process evacuation requests
     while (!pq.empty()) {
         Request req = pq.top();
         pq.pop();
         cout << "Processing request for " << req.zone << " with severity " << req.severity << endl;
     }
-
     return 0;
 }
